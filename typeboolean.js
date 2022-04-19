9e97f2dac242ea48561bd758c6e94fc111def8a8
crawer <dsdouglas810@gmail.com>
2022-04-19 17:43:12 -0300

    primeiros passos com js

---
+++ typeboolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = "Corinthians";
+const texto2 = "Corinthians";
+
+console.log(texto1 === texto2)
---
+++ typenumber.js
@@ -0,0 +1,20 @@
+
+// typer-string 
+
+
+const texto1 = "hello, wordl";
+
+const texto2 = "vai Corinthians";
+
+const senha = "tomatevermelho";
+
+const stringDeNumeros = "1910";
+
+
+    const citacao = "meu nome é ";
+    const MeuNome = "Fabricia";
+    
+ console.log(citacao + MeuNome)
+ 
+
+// concatenacao (+)
